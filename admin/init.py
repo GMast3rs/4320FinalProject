f9420e8b4f6e4f2e88ac12547dd8447cc5a85ab6
Bennet Bradley <37352885+Bradlb03@users.noreply.github.com>
2025-05-08 22:26:05 -0500

    Update init.py

--- admin/init.py
+++ admin/init.py
@@ -2,7 +2,6 @@ from app import db
 
 
 class Admin(db.Model):
-    __tablename__ = 'admin'
-    username = db.Column(db.String, primary_key=True)
+    __tablename__ = 'admins'
+    username = db.Column(db.String, primary_key=True, nullable=False)
     password = db.Column(db.String, nullable=False)
-
