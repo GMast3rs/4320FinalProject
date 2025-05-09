466d0842986c5026a1e3e8c4bb28e5a1cd844a60
Bennet Bradley <37352885+Bradlb03@users.noreply.github.com>
2025-05-08 22:27:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from database import db
+
+db = SQLAlchemy()
 
 def create_app():
+    print("App is running")
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///databases/reservations.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////app/databases/reservations.db"
     app.config["SECRET_KEY"] = "Secretkey"
+    print(app.config["SQLALCHEMY_DATABASE_URI"])
 
     db.init_app(app)
 
-    from admin.init import admin_bp
+
+    from admin.routes import admin_bp
     app.register_blueprint(admin_bp)
 
     @app.route('/')
@@ -17,3 +21,7 @@ def create_app():
         return render_template('index.html')
 
     return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
