253a518e8081e40daa583ca50366624c34e8b3ab
Bennet Bradley <37352885+Bradlb03@users.noreply.github.com>
2025-05-09 21:01:04 -0500

    Update models.py

--- admin/models.py
+++ admin/models.py
@@ -1,11 +1,12 @@
+from datetime import datetime
 from app import db
 
 class Reservation(db.Model):
     __tablename__ = 'reservations'
     
     id = db.Column(db.Integer, primary_key=True)
-    passengerName = db.Column(db.String)
-    seatRow = db.Column(db.Integer)
-    seatColumn = db.Column(db.Integer)
-    eTicketNumber = db.Column(db.String)
-    created = db.Column(db.DateTime)
+    passengerName = db.Column(db.String(100), nullable=False)
+    seatRow = db.Column(db.Integer, nullable=False)
+    seatColumn = db.Column(db.Integer, nullable=False)
+    eTicketNumber = db.Column(db.String(20), nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
